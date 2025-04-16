f1083eef47c2562eac83aca8897b34bf7e072e8c
saikumar pillalamarri <pillalamarrisaikumar8@gmail.com>
2025-04-17 00:33:12 +0530

    Created first server

---
+++ Session-01/index.js
@@ -0,0 +1,8 @@
+const http=require("http");
+const PORT=8082;
+const server=http.createServer(()=>{
+    console.log("Request incoming...");
+})
+server.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
